fa2b5d4f12ed7d3d0ec03a44d5f0e6a8f7a3207a
Victoria Nedelcheva <victoriadim2330@gmail.com>
2023-11-29 21:11:00 +0200

    updating new tasks for the final exam

--- Programming Fundamentals/Final Exam Preparation/Password Reset.py
+++ Programming Fundamentals/Final Exam Preparation/Password Reset.py
@@ -1,3 +1,23 @@
+"""
+tired of it that you decided to help yourself with an intelligent solution.
+Write a password reset program that performs a series of commands upon a predefined string. First, you will receive a string, and afterward, until the command "Done" is given, you will be receiving strings with commands split by a single space. The commands will be the following:
+•	"TakeOdd"
+o	 Takes only the characters at odd indices and concatenates them to obtain the new raw password and then prints it.
+•	"Cut {index} {length}"
+o	Gets the substring with the given length starting from the given index from the password and removes its first occurrence, then prints the password on the console.
+o	The given index and the length will always be valid.
+•	"Substitute {substring} {substitute}"
+o	If the raw password contains the given substring, replace all of its occurrences with the substitute text given and print the result.
+o	If it doesn't, prints "Nothing to replace!".
+Input
+•	You will be receiving strings until the "Done" command is given.
+Output
+•	After the "Done" command is received, print:
+o	"Your password is: {password}"
+Constraints
+•	The indexes from the "Cut {index} {length}" command will always be valid.
+"""
+
 text = input()
 commands_password = input().split()
 
