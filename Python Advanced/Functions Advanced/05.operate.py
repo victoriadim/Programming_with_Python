be6c6e8b2cd306677e748f0d8ee32bdc006eaa39
Victoria Nedelcheva <victoriadim2330@gmail.com>
2024-01-26 22:10:40 +0200

    solution 2

--- Python Advanced/Functions Advanced/05.operate.py
+++ Python Advanced/Functions Advanced/05.operate.py
@@ -10,3 +10,8 @@ def operate(operator, *args):
         return reduce(lambda x, y: x * y, args)
     elif operator == "/":
         return reduce(lambda x, y: x / y, args)
+
+
+# solution 2
+# def operate(operator, *args):
+#     return reduce(lambda x,y: eval(f"{x}{operator}{y}"), args)
