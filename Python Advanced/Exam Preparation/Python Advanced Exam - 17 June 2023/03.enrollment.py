b50ad087dfecd33d54a6b6af9d3be35e17898748
Victoria Nedelcheva <victoriadim2330@gmail.com>
2024-02-07 23:53:31 +0200

    exam prep

--- Python Advanced/Exam Preparation/Python Advanced Exam - 17 June 2023/03.enrollment.py
+++ Python Advanced/Exam Preparation/Python Advanced Exam - 17 June 2023/03.enrollment.py
@@ -13,11 +13,3 @@ def gather_credits(credits, *course_info):
     if credits_shortage >= credits:
         return f"Enrollment finished! Maximum credits: {credits_shortage}.\nCourses: {', '.join(sorted(courses))}"
     return f"You need to enroll in more courses! You have to gather {credits - credits_shortage} credits more."
-
-
-print(gather_credits(
-    80,
-    ("Advanced", 30),
-    ("Basics", 27),
-    ("Fundamentals", 27),
-))
--- Python Advanced/Exam Preparation/Python Advanced Retake Exam - 13 December 2023/02_the_gambler.py
+++ Python Advanced/Exam Preparation/Python Advanced Retake Exam - 13 December 2023/02_the_gambler.py
@@ -1,72 +1,72 @@
-def get_start_pos(matrix):
-    for i, rows in enumerate(matrix):
+money = 100
+steps = {
+    'left': [0, -1],
+    'right': [0, 1],
+    'up': [-1, 0],
+    'down': [1, 0]
+}
+
+
+def get_start_pos(m):
+    for i, row in enumerate(m):
         if 'G' in row:
             for j, pos in enumerate(row):
                 if pos == 'G':
                     return [i, j]
 
 
-def get_next_movement(command, matrix, curr_pos):
-    row, col = curr_pos
-    matrix[row][col] = '-'
-    if command == "up":
-        curr_pos[0] -= 1
-    elif command == "down":
-        curr_pos[0] += 1
-    elif command == "left":
-        curr_pos[1] -= 1
-    elif command == "right":
-        curr_pos[1] += 1
+def get_next(m, s, cp):
+    m[cp[0]][cp[1]] = '-'
+    return [cp[0] + steps[s][0], cp[1] + steps[s][1]]
 
 
-def get_value(matrix, curr_pos):
-    return matrix[curr_pos[0]][curr_pos[1]]
+def get_value(m, cp):
+    return m[cur_pos[0]][cp[1]]
 
 
-def update_money(cur_char, money, if_jackpot):
-    if cur_char == 'P':
-        money -= 200
-    elif cur_char == 'W':
-        money += 100
-    elif cur_char == 'J':
-        if_jackpot = True
-        money += 100000
-    return money
+def update_money(value, m):
+    if value == 'P':
+        m -= 200
+    elif value == 'W':
+        m += 100
+    elif value == 'J':
+        m += 100000
+    return m
 
 
 size = int(input())
-money = 100
-matrix_size = []
-jackpot = False
+matrix = []
 
 for _ in range(size):
-    matrix_size.append(list(input()))
+    matrix.append(list(input()))
 
-starting_position = get_start_pos(matrix_size)
-
-for directions in range(size):
-    if directions == 'end':
-        matrix_size[starting_position[0]][starting_position[1]] = 'G'
-        print(f'Finish game. Total amount: {money}')
+cur_pos = get_start_pos(matrix)
+while True:
+    step = input()
+    if step == 'end':
+        matrix[cur_pos[0]][cur_pos[1]] = 'G'
+        print(f'End of the game. Total amount: {money}$')
         break
-    cur_pos = get_next_movement(matrix_size, command, cur_pos)
-    if cur_pos[0] < 0 or cur_pos[1] < 0 or cur_pos[0] > len(matrix_size) or cur_pos[1] > len(matrix_size):
+
+    cur_pos = get_next(matrix, step, cur_pos)
+
+    if cur_pos[0] < 0 or cur_pos[1] < 0 or cur_pos[0] > len(matrix) or cur_pos[1] > len(matrix):
         money = 0
         print('Game over! You lost everything!')
         break
 
-    pos_value = get_value(matrix_size, cur_pos)
+    pos_value = get_value(matrix, cur_pos)
     money = update_money(pos_value, money)
 
     if money <= 0:
-        matrix_size[cur_pos[0]][cur_pos[1]] = 'G'
+        matrix[cur_pos[0]][cur_pos[1]] = 'G'
         print('Game over! You lost everything!')
         break
     elif pos_value == 'J':
-        matrix_size[cur_pos[0]][cur_pos[1]] = 'G'
-        print('You win the Jackpot!')
-        print(f'End of the game. Total amount: {money}')
+        matrix[cur_pos[0]][cur_pos[1]] = 'G'
+        print(f'You win the Jackpot!\nEnd of the game. Total amount: {money}$')
         break
+
 if money > 0:
-    for row in matrix_size:
+    for row in matrix:
         print(''.join(row))
