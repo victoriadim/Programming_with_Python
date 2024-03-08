bf548c676e810c16978ab18092f5115eace4563c
Victoria Nedelcheva <victoriadim2330@gmail.com>
2024-03-09 00:09:16 +0200

    exercise

--- Python OOP/Encapsulation - Exercise/03.football_team_generator/project/team.py
+++ Python OOP/Encapsulation - Exercise/03.football_team_generator/project/team.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 from project.player import Player
 
 
@@ -13,3 +15,10 @@ class Team:
         self.__players.append(player)
         return f"Player {player.name} joined team {self.__name}"
 
+    def remove_player(self, player_name: str) -> Union[Player, str]:
+        try:
+            player = [p for p in self.__players if p.name == player_name][0]
+            self.__players.remove(player)
+            return player
+        except IndexError:
+            return f"Player {player_name} not found"
